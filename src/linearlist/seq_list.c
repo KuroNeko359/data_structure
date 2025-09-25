c7167b71f7ce3861df9ca8af7f46b93bdccef392
kuroneko359 <2077056966@qq.com>
2025-09-25 18:00:22 +0800

    Fixed some bug.

--- src/linearlist/seq_list.c
+++ src/linearlist/seq_list.c
@@ -10,6 +10,8 @@
 
 #include "log.h"
 
+#define TYPE_SIZE sizeof(seq_list_type)
+
 void seq_print(seq_list *seq) {
     int i;
     for (i = 0; i < seq->len; i++) {
@@ -18,20 +20,33 @@ void seq_print(seq_list *seq) {
     printf("\n");
 }
 
+static void arrcpy(int *dest, int *src, int elem_count) {
+    memcpy(
+        dest,
+        src,
+        elem_count * TYPE_SIZE
+    );
+    if (ENABLE_DEBUG) {
+        printf("Array copy: from address %p to %p, element count: %d \n", src, dest, elem_count);
+    }
+}
+
 seq_list *seq_list_init(seq_list_type *arr, unsigned int arr_size) {
     seq_list *seq = malloc(sizeof(seq_list));
-    seq->elem = (seq_list_type *) malloc(arr_size * sizeof(seq_list_type));
-    memcpy(seq->elem, arr, arr_size * sizeof(seq_list_type));
+    seq->elem = (seq_list_type *) malloc(arr_size * TYPE_SIZE);
+    arrcpy(seq->elem, arr, arr_size);
     seq->len = arr_size;
     seq->size = arr_size;
     return seq;
 }
 
 void seq_realloc(seq_list *seq) {
-    void *new_pointer = realloc(seq->elem, seq->size * 2);
+    void *new_pointer = realloc(seq->elem, 2 * seq->size * TYPE_SIZE);
     seq->elem = new_pointer;
     seq->size = seq->size * 2;
-    DEBUG_PRINT("Seq has reallocated, now seq->size is %d.", seq->size);
+    if (ENABLE_DEBUG) {
+        DEBUG_PRINT("Seq has reallocated, now seq->size is %d.", seq->size);
+    }
 }
 
 void seq_add_elem(seq_list *seq,seq_list_type elem) {
@@ -48,25 +63,29 @@ void seq_add_elem(seq_list *seq,seq_list_type elem) {
 }
 
 void seq_append_elems(seq_list *seq,seq_list_type *arr, unsigned int arr_size) {
-    if (seq->len + arr_size > seq->size) {
-        // TODO Fix the case that seq->len + arr_size > 2 * seq->size
+    printf("%d\n", *(seq->elem + 4));
+    while (seq->len + arr_size > seq->size) {
         seq_realloc(seq);
     }
-    seq_list_type *seq_end = (seq->elem + seq->len);
-    memcpy(seq_end, arr, arr_size * sizeof(seq_list_type));
+    seq_list_type *seq_end = seq->elem + seq->len;
+    printf("%d\n", *(seq->elem + 4));
+    arrcpy(
+        seq_end,
+        arr,
+        arr_size
+    );
+    printf("%d\n", *(seq->elem + 4));
+    if (ENABLE_DEBUG) {
+        printf("memcpy: from %p to %p, len:%d \n", arr, seq_end, arr_size);
+    }
     seq->len += arr_size;
 }
 
 void seq_delete_elem(seq_list *seq, int index) {
-    memcpy(
+    arrcpy(
         seq->elem + index,
         seq->elem + index + 1,
-        (seq->len - (unsigned int)(index + 1)) * sizeof(seq_list_type)
-    );
-    if (ENABLE_DEBUG) {
-        printf("memcpy: from %p to %p, len:%d \n",seq->elem + index + 1,seq->elem + index ,(seq->len - (unsigned int)(index + 1)) );
-    }
-
+        (seq->len - (unsigned int)(index + 1)));
     seq->len--;
 }
 
@@ -75,14 +94,11 @@ void seq_insert_elem(seq_list *seq, int index,seq_list_type elem) {
     if (seq->len + 1 >= seq->size) {
         seq_realloc(seq);
     }
-    memcpy(
+    arrcpy(
         seq->elem + index + 1,
         seq->elem + index,
-        (seq->len - (unsigned int)index) * sizeof(seq_list_type)
+        (seq->len - (unsigned int)index)
     );
-    if (ENABLE_DEBUG) {
-        printf("memcpy: from %p to %p, len:%d \n",seq->elem + index,seq->elem + index + 1,(seq->len - (unsigned int)index) );
-    }
     *(seq->elem + index) = elem;
 
     seq->len++;
@@ -92,6 +108,6 @@ seq_list_type seq_get_elem(seq_list *seq, int index) {
     if (index < seq->size) {
         return *(seq->elem + index);
     }
-    printf("Index %d is out of bounds.",index);
+    printf("Index %d is out of bounds.", index);
     exit(1);
 }
