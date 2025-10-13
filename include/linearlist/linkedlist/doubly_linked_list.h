cbb263387fbecb77e2037c710327ab2b7b585c44
kuroneko359 <2077056966@qq.com>
2025-10-13 22:21:10 +0800

    Added comments for any functions.

--- include/linearlist/linkedlist/doubly_linked_list.h
+++ include/linearlist/linkedlist/doubly_linked_list.h
@@ -1,28 +1,59 @@
 //
 // Created by KuroNeko359 on 2025/9/21.
 //
-#include "types.h"
 
 #ifndef CSAPP_DOUBLY_LINKED_LIST_H
 #define CSAPP_DOUBLY_LINKED_LIST_H
 
 #define DOUBLY_LIST_ELEM_TYPE int
 
+#include "types.h"
+
+
 typedef struct doubly_list_node {
     DOUBLY_LIST_ELEM_TYPE data;
     struct doubly_list_node *prev, *next;
 } doubly_list_node, doubly_linked_list;
 
+/**
+ * Print list.
+ * @param head_node Head of the linked list.
+ */
 void doubly_list_print(doubly_linked_list *head_node);
 
-void doubly_list_append(doubly_linked_list *head_node,DOUBLY_LIST_ELEM_TYPE data);
+/**
+ * Append a node for this linked list.
+ * @param head_node Head of the linked list.
+ * @param data The data of the new node.
+ */
+void doubly_list_append(doubly_linked_list *head_node,
+                        DOUBLY_LIST_ELEM_TYPE data);
 
-doubly_list_node *doubly_list_create(DOUBLY_LIST_ELEM_TYPE data);
+/**
+ * Create a new node.
+ * @param data The data of the new node.
+ * @return The pointer of the new node.
+ */
+doubly_list_node *doubly_list_create_node(DOUBLY_LIST_ELEM_TYPE data);
 
-doubly_list_node *doubly_list_init(DOUBLY_LIST_ELEM_TYPE data);
+/**
+ * Initialize a doubly linked list.
+ * @return The head pointer of the head node.
+ */
+doubly_list_node *doubly_list_init();
 
+/**
+ * Get tail node of the linked list.
+ * @param head The pointer of head node.
+ * @return The pointer of tail node.
+ */
 doubly_list_node *doubly_list_get_tail(doubly_linked_list *head);
 
+/**
+ * Verify whether this linked list is empty.
+ * @param head The head of the linked list.
+ * @return If this linked list is empty, return true.
+ */
 bool doubly_linked_list_is_empty(doubly_linked_list *head);
 
 #endif //CSAPP_DOUBLY_LINKED_LIST_H
